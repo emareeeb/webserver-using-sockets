bf44a008ae863352f89ca2c40bc8d301743ec109
Areeb Ahmed <132186043+emareeeb@users.noreply.github.com>
2024-03-05 19:36:15 +0530

    initial commit

---
+++ server.py
@@ -0,0 +1,35 @@
+import socket
+import ssl
+import threading
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+    connected = True
+    while connected:
+        msg = conn.recv(1024).decode()
+        if not msg:
+            break
+        print(f"[{addr}] {msg}")
+        response = "Message received by server."
+        conn.send(response.encode())
+    conn.close()
+
+def main():
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    context.load_cert_chain('certificate.crt', 'private.key')
+    
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ssl_server_socket = context.wrap_socket(server_socket, server_side=True)
+    ssl_server_socket.bind(('192.168.56.1', 5566))
+    ssl_server_socket.listen(5)
+    print("[LISTENING] Server is listening on 192.168.56.1:5566")
+    
+    while True:
+        conn, addr = ssl_server_socket.accept()
+        
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+
+if __name__ == "__main__":
+    main()
